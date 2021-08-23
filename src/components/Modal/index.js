c1ae899800d9644708e9f2d0890dfd72364b2a3b
Giulia Santos <giulia@versatushpc.com.br>
2021-08-23 02:02:56 -0300

    Prop onClose for close Modal

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import {ModalStyled, CardModalStyled} from './style'
 import { CloseButton } from '../Buttons'
 
-export default function Modal({children}) {
+export default function Modal({children, onClose}) {
     return(
         <ModalStyled>
             <CardModalStyled>
-                <CloseButton/>
+                <CloseButton onClick={onClose}/>
                 <div>
                     {children}
                 </div>
